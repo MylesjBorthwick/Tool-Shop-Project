49934623d43b281c33758e29d6bc0a7fc7a7a4a9
kenloughery <64917859+kenloughery@users.noreply.github.com>
2020-11-24 17:15:51 -0700

    Controller and model for customer done

--- ToolShopProject/L8Q3/src/ClientManager.java
+++ ToolShopProject/L8Q3/src/ClientManager.java
@@ -2,10 +2,11 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
-//import java.sql.PreparedStatement;
+//import java.sql.PreparedStatement; 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 // Pre-Project Exercise 
@@ -167,6 +168,33 @@ public class ClientManager {
 		return null;
 	}
 
+
+	public ArrayList<Client> getAll()
+	{
+		ArrayList<Client> list = new ArrayList<Client>();
+		String sql = "SELECT * FROM " + tableName;
+		ResultSet client;
+		try {
+			statement = jdbc_connection.createStatement();
+			client = statement.executeQuery(sql);
+			while(client.next())
+			{
+				list.add( new Client(client.getInt("ID"),
+								  client.getString("FIRSTNAME"),
+								  client.getString("LASTNAME"),
+								  client.getString("ADDRESS"),
+								  client.getString("POSTALCODE"),
+								  client.getString("PHONENUMBER"),
+								  client.getString("CLIENTTYPE")));
+			}
+		
+		} catch (SQLException e) { e.printStackTrace(); }
+		
+		return list;
+	}
+
+
+
 	// Prints all the items in the database to console
 	public void printTable()
 	{
@@ -226,7 +254,7 @@ public class ClientManager {
 		
 		//System.out.println("\nTrying to remove the table");
 		//clientList.removeTable();
-		
+
 		try {
 			clientList.statement.close();
 			clientList.jdbc_connection.close();
---
+++ ToolShopProject/L8Q3/src/CustomerList.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+/**
+ * @author Myles Borthwick
+ * @version 1.0
+ */
+
+public class CustomerList {
+
+    private ArrayList<Client> customerList;
+    /**
+     * Constructor
+     * @param suppliers
+     */
+    public CustomerList(ArrayList<Client> clients){
+        setCustomerList(clients);
+    }
+
+    /**
+     * Print supplier list
+     */
+    public String printList(){
+        String s = "";
+        for(Client sup:customerList){
+            s+= sup;
+            s += "\n";
+        }
+        return s;
+    }
+
+    /**
+     * @return ArrayList<Supplier> return the supplierList
+     */
+    public ArrayList<Client> getCustomerList() {
+        return customerList;
+    }
+
+    /**
+     * @param supplierList the supplierList to set
+     */
+    public void setCustomerList(ArrayList<Client> customerList) {
+        this.customerList = customerList;
+    }
+
+}
